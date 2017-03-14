8500c3d7b637c57013b6b272e3a16929a01ec1d0
Matthew McLean <matthewmclean21@gmail.com>
2017-03-14 15:49:22 -0700

    Added file organization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using std::ifstream;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main(){
+
+  string test1 = "graphics/setup.txt";
+  string test2 = "graphics/basic-room.txt";
+
+  ifstream textImg;
+  char input;
+
+  for(int i = 0; i < 800; i++){
+    cout << endl;
+  }
+
+  cout << endl;
+  textImg.open(test1);
+  while(textImg.get(input)){
+    std::cout << input;
+  }
+  textImg.close();
+  cout << endl;
+
+  cin.get();
+
+  for(int i = 0; i < 800; i++){
+    cout << endl;
+  }
+
+  cout << endl;
+  textImg.open(test2);
+  while(textImg.get(input)){
+    std::cout << input;
+  }
+  textImg.close();
+  cout << endl;
+
 
   return 0;
 }
